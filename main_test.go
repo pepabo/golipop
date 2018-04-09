96c87f4fca766f405760dec78be541b198748a84
linyows <linyows@gmail.com>
2018-04-09 09:27:55 +0900

    add fixture method for test

--- main_test.go
+++ main_test.go
@@ -2,10 +2,13 @@ package lolp
 
 import (
 	"bytes"
-	"io"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -16,19 +19,12 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func loginHandler() func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`eyJ0eXAiOiJKV1QiLCJ.eyJzdWIiOiIxOTg4M2VjZi0yYWUyLTRmZmUtYjg1MS03MzMTgwMDMsImV4cCI6MT2MzcxYyJ9.Jm6Pzn4bXFHLHy2HNqJY`))
+func fixture(ctx string, r *http.Request) string {
+	f := fmt.Sprintf("%s--%s.json", r.Method, ctx)
+	p := filepath.Join("testdata", r.URL.String(), f)
+	b, err := ioutil.ReadFile(p)
+	if err != nil {
+		panic("fixture not found: " + p)
 	}
-}
-
-func projectCreateHandler() func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		io.WriteString(w, `{"domain": "foobar-baz-9999.lolipop.io"}`)
-		//w.Write([]byte(`{"kind":"wordpress","payload":{"email":"foo@example.com","password":"Secret#Gopher123?","username":"foo"}}`))
-	}
+	return strings.TrimSuffix(string(b), "\n")
 }
