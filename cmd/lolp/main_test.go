0b077466440c99db57b942cf5e6ca8cf1ab2ff2f
linyows <linyows@gmail.com>
2018-04-09 16:06:53 +0900

    add test for cli

--- cmd/lolp/main_test.go
+++ cmd/lolp/main_test.go
@@ -1,6 +1,26 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
 
-func TestCLI(t *testing.T) {
+	"github.com/pepabo/golipop"
+)
+
+func TestVersion(t *testing.T) {
+	out, err := new(bytes.Buffer), new(bytes.Buffer)
+
+	cli := &CLI{outStream: out, errStream: err}
+	args := strings.Split("lolp --version", " ")
+
+	if status := cli.run(args); status != ExitOK {
+		t.Fatalf("expected: \"%d\", actual: \"%d\"", ExitErr, status)
+	}
+
+	expected := fmt.Sprintf("lolp version %s", lolp.Version)
+	if !strings.Contains(err.String(), expected) {
+		t.Fatalf("expected: \"%s\", actual: \"%s\"", expected, err.String())
+	}
 }
